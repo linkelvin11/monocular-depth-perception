8aaf9e58c49a8f513aa6a54e61bfef99ed16d43c
Kelvin Lin <lin.kelvin11@gmail.com>
2017-04-30 14:54:15 -0400

    split training/validation in filereader

--- files.py
+++ files.py
@@ -2,74 +2,78 @@
 
 import tensorflow as tf
 import random
+import numpy as np
 
+def create_input_pipeline_from_files_list(image_list, label_list, batch_size):
+    filename_queue = tf.train.slice_input_producer([
+        image_list
+        , label_list
+        ] , shuffle=True)
+
+    # Decode image png
+    image_file_content = tf.read_file(filename_queue[0])
+    image = tf.image.decode_png(image_file_content, channels = 3)
+
+    # Decode label png
+    label_file_content = tf.read_file(filename_queue[1])
+    label = tf.image.decode_png(label_file_content, channels = 1, dtype=tf.uint16)
+
+    # Crop the image to the desired size
+    image_crop_shape = tf.concat([crop_shape, [3]], 0)
+    label_crop_shape = tf.concat([crop_shape, [1]], 0)
+    rand_seed = random.random()*10000
+    cropped_image = tf.random_crop(image, image_crop_shape, seed=rand_seed)
+    cropped_image = tf.cast(cropped_image, tf.float32)/255.
+    # Crop the label to the desired size
+    if __name__ == "__main__":
+        cropped_label = tf.random_crop(image, image_crop_shape, seed=rand_seed)
+        cropped_label = tf.cast(cropped_label, tf.float32)/255.
+    else:
+        max_dist = 2000.
+        cropped_label = tf.random_crop(label, label_crop_shape, seed=rand_seed)
+        cropped_label = tf.cast(cropped_label, tf.float32)
+        cropped_label = tf.clip_by_value(cropped_label, 0, max_dist)/max_dist
+
+
+    # Create a batch
+    image_batch, label_batch = tf.train.batch([
+        cropped_image
+        , cropped_label
+        ], batch_size=batch_size)
+    return [image_batch, label_batch]
 
 class FileReader():
     """
-    FileReader manages all the queueing involved in loading images from files, taking random crops,
-    and forming batches.
+    FileReader performs the cropping and batching for input files
     """
 
     def __init__(self, image_glob, label_glob, crop_shape, batch_size=10, dtype=tf.float32):
         """
-        Creates a FileReader that matches image filenames with `glob`,
-        crops them to `crop_shape`, and produces batches of size `batch_size`.
+        Creates a filereader which produces batches from a list of images and a list of labels
         """
-        # Create a queue of all the filenames
-        # print ('matching filenames')
-        # all_image_filenames = tf.train.match_filenames_once(image_glob)
-        # all_label_filenames = tf.train.match_filenames_once(label_glob)
-        # self.batch = [all_image_filenames, all_label_filenames]
-        # return
-        # print ('done matching filenames')
 
-        filename_queue = tf.train.slice_input_producer([
-            image_glob
-            , label_glob
-            ] , shuffle=True)
+        np.random.seed(seed=1234)
+        partition = np.random.binomial(1, 0.01, size=[len(image_glob)])
+        np.random.seed()
 
-        # Decode image png
-        image_file_content = tf.read_file(filename_queue[0])
-        image = tf.image.decode_png(image_file_content, channels = 3)
+        train_images, val_images = tf.dynamic_partition(image_glob, partition, 2)
+        train_labels, val_labels = tf.dynamic_partition(label_glob, partition, 2)
 
-        # Decode label png
-        label_file_content = tf.read_file(filename_queue[1])
-        label = tf.image.decode_png(label_file_content, channels = 1, dtype=tf.uint16)
+        self.batch = create_input_pipeline_from_files_list(train_images, train_labels, batch_size)
 
-        # Create a reader and decode a jpeg image
-        # image_reader = tf.WholeFileReader()
-        # _, image_file = image_reader.read(filename_queue)
-        # image = tf.image.decode_jpeg(image_file, channels=3)
-
-        # Crop the image to the desired size
-        image_crop_shape = tf.concat([crop_shape, [3]], 0)
-        label_crop_shape = tf.concat([crop_shape, [1]], 0)
-        rand_seed = random.random()*10000
-        cropped_image = tf.random_crop(image, image_crop_shape, seed=rand_seed)
-        cropped_image = tf.cast(cropped_image, dtype)/255.
-        # Crop the label to the desired size
-        if __name__ == "__main__":
-            cropped_label = tf.random_crop(image, image_crop_shape, seed=rand_seed)
-            cropped_label = tf.cast(cropped_label, dtype)/255.
-        else:
-            max_dist = 2000.
-            cropped_label = tf.random_crop(label, label_crop_shape, seed=rand_seed)
-            cropped_label = tf.cast(cropped_label, dtype)
-            cropped_label = tf.clip_by_value(cropped_label, 0, max_dist)/max_dist
-
-
-        # Create a batch
-        image_batch, label_batch = tf.train.batch([
-            cropped_image
-            , cropped_label
-            ], batch_size=batch_size)
-        self.batch = [image_batch, label_batch]
+        self.val_batch = create_input_pipeline_from_files_list(val_images, val_labels, batch_size)
 
     def get_batch(self):
         """
         Returns a batch.
         """
         return self.batch
+
+    def get_val_batch(self):
+        """
+        Returns a validation batch
+        """
+        return self.val_batch
 
     def start_queue_runners(self):
         """
